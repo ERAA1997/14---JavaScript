76a8ff895e089a0e749875bec789bdfedffde952
Eoghan <altoneoghan@yahoo.com>
2024-02-03 19:20:24 +0000

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,139 +1,87 @@
 
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-
-// d3.json(url).then(function(data){  
-
-//     console.log(data);
-
-//     let samples = data.samples;
-    
-//     console.log(samples);
-// });
-
-
 function optionChanged(subjectID) {
     console.log(subjectID);
+    barchart(subjectID);
+    metadata(subjectID);
 }
-
-function startUp() {
-    let dropDown = d3.select("#selDataset");
-
-    d3.json(url).then(function(data){  
-
-    
-        let names = data.names;
-
-        names.forEach(element => { 
-            dropDown.append("option").attr("value", element).text(element);
-        });
-        let firstSubjectId = names[0];
-        optionChanged(firstSubjectId);
-    });
-};
-
-
-
-startUp();
-
-
-// d3.selectAll("#selDataset").on("change",updatePlotly);
-
-// function updatePlotly() {
-//     let dropdownMenu = d3.select("#selDataset");
-
-//     let dataset = dropdownMenu.property("value")
-// }
-
-
-//sort
 
 function metadata(sample) {
+    d3.json(url).then((data) => {
+        let entireData = data.metadata;
+        let barX = entireData.filter(sampleObj => sampleObj.id == sample);
+        let emptyArray = barX[0];
+        let PANEL = d3.select("#sample-metadata");
 
-d3.json(url).then((data) =>{
+        PANEL.html("");
+        for (key in emptyArray) {
+            PANEL.append("h6").text(`${key.toUpperCase()}: ${emptyArray[key]}`);
+        };
 
-let entireData = data.samples;
-
-let barX = entireData.filter(sampleObj => sampleObj.id === sample);
-
-let emptyArray = barX[0];
-
-let PANEL = d3.select("#sample-metadata")
-
-PANEL.html("hr")
-
-for (key in emptyArray) {
-    PANEL.append("h6").text("${key.toUppercase()}: ${result[key]}")
+        buildGauge(emptyArray.wfreq);
+    });
 }
-buildGauge(result.wfreq);
-});
-};
 
 function barchart(sample) {
+    d3.json(url).then((data) => {
+        let entireData = data.samples;
+        let barX = entireData.filter(sampleObj => sampleObj.id === sample);
+        let emptyArray = barX[0];
 
-d3.json(url).then((data) =>{
+        let layout = {
+            title: 'Bubble',
+            hovermode: 'closest',
+            margin: { t: 0 },
+            margin: { t: 30 }
+        };
 
-let entireData = data.samples;
+        let trace1 = {
+            x: emptyArray.otu_ids,
+            y: emptyArray.sample_values,
+            text: emptyArray.otu_labels,
+            mode: 'markers',
+            marker: {
+                size: emptyArray.sample_values,
+                color: emptyArray.otu_ids,
+                colorscale: 'earth'
+            }
+        };
 
-let barX = entireData.filter(sampleObj => sampleObj.id === sample);
+        let data1 = [trace1];
 
-let emptyArray = barX[0];
+        Plotly.newPlot('bubble', data1, layout);
 
-let otu_ids1 = emptyArray.otu_ids1;
-console.log(barX);
+        let yaxis = emptyArray.otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse()
 
-let otu_labels1 = emptyArray.otu_labels1;
+        let trace2 = {
+            x: emptyArray.sample_values.slice(0, 10).reverse(),
+            y: yaxis,
+            type: 'bar',
+            orientation: 'h',
+        };
 
-let sample_values1 = emptyArray.sample_values1;
+        let data2 = [trace2];
 
+        Plotly.newPlot('bar', data2);
+    });
+}
 
-let layout = {
-    title:'Bubble',
-    hovermode:'closest',
-    margin: {t: 0},
-    xaxis: {title: 'test'},
-    margin:{t:30}
-};
+function init() {
+    let selection = d3.select("#selDataset");
 
-let trace1 = {
-    x:otu_ids1,
-    y:sample_values1,
-    text:otu_labels1,
-    mode:"markers",
-    marker:{
-        size: sample_values1,
-        colour: otu_ids1,
-        colourscale:"earth"
-        }
-  
-};
+    d3.json(url).then((data) => {
+        let names = data.names;
 
-let data1 = [trace1];
+        names.forEach(element => {
+            selection.append("option").attr("value", element).text(element);
+        });
 
-Plotly.newPlot("bubble", data1, layout);
+        let firstSubjectId = names[0];
+        barchart(firstSubjectId);
+        metadata(firstSubjectId);
+    });
+}
 
-// });
-
-// let trace2 = {
-//      x:otu_ids1,
-//      y:sample_values1,
-//      type: "bar",
-//      orientation: 'h',
-// };
-// let data2 = [trace2];
-
-// Plotly.newPlot("bar", data2);
-
-});
-
-};
-
-// for sorting const top10Data = sortedData.slice(0, 10);
-
-// const sortedData = otuData.sort((a, b) => b.value - a.value);
-
-// const otuNames = top10Data.map(entry => entry.otu);
-
-// const filteredData = data.filter(item => item.id === desiredId);
-
+init();
 
